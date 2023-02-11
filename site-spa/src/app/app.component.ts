e1ab0dabf8161851171d9aea83f85457bbd1adc5
Daniel Silva <daniel.silva@onlineapp.com.br>
2023-02-11 16:00:03 -0300

    Install bootstrap

--- site-spa/src/app/app.component.ts
+++ site-spa/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'site-spa';
+
+  animal = {
+    name: "Leão",
+    image: "https://i.pinimg.com/originals/5d/81/af/5d81af68bc4a0de97f58b6799d606b5c.jpg",
+  }
 }
