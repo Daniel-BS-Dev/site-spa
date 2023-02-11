cc70e5a532070a7374f9f620d34e2b41163b8cf4
Daniel Silva <daniel.silva@onlineapp.com.br>
2023-02-11 18:07:05 -0300

    create compoment and service photos

--- site-spa/src/app/app.module.ts
+++ site-spa/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListPhotosComponent } from './photos/list-photos/list-photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPhotosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ site-spa/src/app/photos/list-photos/list-photos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-photos',
+  templateUrl: './list-photos.component.html',
+  styleUrls: ['./list-photos.component.scss']
+})
+export class ListPhotosComponent {
+
+}
