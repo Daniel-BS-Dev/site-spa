a62216dfe4a2bf778469dff6eb388dd522bbec32
Daniel Silva <daniel.silva@onlineapp.com.br>
2023-02-12 16:16:17 -0300

    created header component

--- site-spa/src/app/app.module.ts
+++ site-spa/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { SignModule } from './sign/sign.module';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
