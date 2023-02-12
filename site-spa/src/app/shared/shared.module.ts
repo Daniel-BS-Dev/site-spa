a3dfd11c206adb37a2ba5cd3a211ff3f2dba1fec
Daniel Silva <daniel.silva@onlineapp.com.br>
2023-02-12 11:38:01 -0300

    created card components

--- site-spa/src/app/shared/shared.module.ts
+++ site-spa/src/app/shared/shared.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SearchingComponent } from './searching/searching.component';
 import { LoadingComponent } from './loading/loading.component';
+import { CardComponent } from './card/card.component';
 
 const declarations = [
   SearchingComponent,
-  LoadingComponent
+  LoadingComponent,
+  CardComponent
 ];
 
 @NgModule({
